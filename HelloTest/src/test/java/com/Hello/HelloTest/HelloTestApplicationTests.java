23e2c8ccb975a2694aaac31b5443322af8710072
Akshat Arora <AkshatArora@corp.fairisaac.com>
2019-09-24 17:47:20 +0530

    JunitTest

--- HelloTest/src/test/java/com/Hello/HelloTest/HelloTestApplicationTests.java
+++ HelloTest/src/test/java/com/Hello/HelloTest/HelloTestApplicationTests.java
@@ -1,7 +1,10 @@
 package com.Hello.HelloTest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +14,13 @@ public class HelloTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	@Autowired 
+	public HelloWorld helloworld;
+	@Test
+	public void test() {
+		String str=helloworld.home();
+		assertEquals(str,"Hello World!");
 	}
 
 }
